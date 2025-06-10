cfdc48e25fa12f7eb0a493cb92264bf11da00ddf
AMIREDDYRAKEY <rakeyr213@gmail.com>
2025-06-10 23:57:21 +0530

    Events

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import Coordinators from './pages/Coordinators';
 import About from './pages/About';
 import Contact from './pages/Contact'
 import Login from './pages/Login';
-import Userlogin from './components/Userlogin ';
-import Individualevent from './components/Individualevent';
+import Userlogin from './components/Userlogin';
 const App = () => {
   return (
     <div>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -69,7 +69,7 @@ const Cards = ({ limit }) => {
             <div className='flex mt-[20px] lg:ml-[15px] ml-[20px] lg:gap-4 gap-5 font-semibold text-sm text-white'>
               <button
                 className='border border-[#8162c0] h-[30px] w-[100px] rounded-lg'
-                 onClick={()=>navigate('/Events/:${event.id}') }
+                 onClick={()=>setdetails(!details) }
               >
                 Viewdetails
               </button>
