a3866cf657e027c3f2dbd6061e8d0cdd8ec7e07f
AMIREDDYRAKEY <rakeyr213@gmail.com>
2025-06-10 22:15:42 +0530

    Events

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -6,8 +6,8 @@ const Sidenav = ({setsidenav}) => {
  
 
   return (
-<div className="h-screen w-[220px] bg-[#a32cc4] text-white shadow-lg p-6 rounded-r-xl fixed top-0 left-0 z-50">
-     <div className='flex items-end justify-end mt-[20px]' ><button onClick={()=>{setsidenav(false)}} ><IoCloseSharp className='text-black ' /></button></div>
+<div className="h-screen w-[220px] bg-gradient-to-br from-[#27164d] via-[#581572] to-[#4e032e] text-white shadow-lg p-6 rounded-r-xl fixed top-0 left-0 z-50">
+     <div className='flex items-end justify-end mt-[20px]' ><button onClick={()=>{setsidenav(false)}} ><IoCloseSharp className='text-white ' /></button></div>
       <div className="flex flex-col gap-10 font-semibold text-white  text-2xl mt-[80px] ">
         <button
           onClick={() => navigate('/')}
