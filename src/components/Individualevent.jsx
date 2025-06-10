1e731f3c27bc970336ed407958bf21200a258068
AMIREDDYRAKEY <rakeyr213@gmail.com>
2025-06-10 17:13:13 +0530

    Events

--- src/components/Individualevent.jsx
+++ src/components/Individualevent.jsx
@@ -30,14 +30,7 @@ const Individualevent = ({ details, setdetails }) => {
   return (
     <div className="flex flex-col lg:justify-center z-100 lg:items-center">
       <div className="lg:flex lg:flex-row flex flex-col gap-6 px-6 py-4 lg:w-[600px]  bg-[#131e4e] rounded-lg shadow-md text-white">
-        <button
-          className="absolute lg:pl-[520px] pl-[230px]   mt-[-15px] lg:mt-[-1px] text-white font-bold text-xl"
-          onClick={() => setdetails(!details)}
-
-        >
-          <IoCloseSharp />
-
-        </button>
+ 
         {/* Image */}
         <div>
           <img
@@ -67,6 +60,14 @@ const Individualevent = ({ details, setdetails }) => {
             >
               Results
             </button>
+                    <div className='ml-[80px] lg:mt-0 mt-[-240px]'><button
+          className=" text-2xl  mt-[-15px] lg:mt-[-1px] text-white font-bold "
+          onClick={() => setdetails(!details)}
+
+        >
+          <IoCloseSharp />
+
+        </button></div>
           </div>
 
           {/* Schedule Section */}
