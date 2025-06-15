a6a767a5bbca03cdbf843638f0d1e2f3e0b412b5
AMIREDDYRAKEY <rakeyr213@gmail.com>
2025-06-15 13:59:20 +0530

    Events

--- src/components/Eventnav.jsx
+++ src/components/Eventnav.jsx
@@ -11,7 +11,7 @@ const Eventnav = () => {
     <div className="min-h-screen  pb-[50px]  lg:pt-[100px] pt-[60px] flex flex-col items-center justify-center  bg-[#12122d]">
       <div className='w-[100%] '><Nav /></div>
       <div className='  pt-[50px] gap-4'>
-        <h1 class="text-4xl md:text-4xl text-transparent text-[#f033d7] animate-gradient-x text-center font-bold">
+        <h1 class="text-4xl md:text-4xl text-[#f033d7] animate-gradient-x text-center font-bold">
           Tech Explore Events
         </h1>
 
