d19a95112075064435b1cc48415c7c520aa46006
AMIREDDYRAKEY <rakeyr213@gmail.com>
2025-06-23 17:39:00 +0530

    first commit

--- src/components/Userlogin.jsx
+++ src/components/Userlogin.jsx
@@ -4,24 +4,15 @@ import { IoCloseSharp } from "react-icons/io5";
 const Userlogin = () => {
   const [sub, setsub] = useState('')
   const [open,setopen]=useState('')
-  const [username,setusername]=useState('')
   const [password ,setpassword]=useState('')
-  const [Idno,setIdno]=useState('')
+  const [Confirmpassword,setConfirmpassowrd]=useState('')
   const [errors,seterrors]=useState({
-    username:'',
     password:'',
-    Idno:''
+   Confirmpassword:''
   });
   const handlesubmit =(e)=>{
     e.preventDefault();
     let isValid=true
-    if(username.trim()===""){
-      seterrors((errors) =>({...errors,username:'Required Username'}) )
-      isValid=false
-    }
-    else{
-      seterrors((errors) =>({...errors,username:''}) )
-    }
     // Passowrd
     if(password.trim()===""){
       seterrors((errors)=>({...errors,password:'Password Required'}))
@@ -33,18 +24,18 @@ const Userlogin = () => {
     else{
       seterrors((errors)=>({...errors,password:''}))
     }
-    // Idno
-    if(Idno.trim()===""){
-      seterrors((errors)=>({...errors,Idno:'Id No is Required'}))
+    // Confrim Password
+    if(Confirmpassword.trim()===""){
+      seterrors((errors)=>({...errors,Confirmpassword:'Id No is Required'}))
     }
     else{
-      seterrors((errors)=>({...errors,Idno:''}))
+      seterrors((errors)=>({...errors,Confirmpassword:''}))
     }
     if(isValid){
       setopen(true)
       setusername('')
       setpassword('')
-      setIdno('')
+      setConfirmpassowrd('')
     }
     else{
       setopen(false)
@@ -68,14 +59,12 @@ const Userlogin = () => {
               <h3 className="text-4xl text-white font-semibold uppercase mt-[10px]">Login </h3>
 
               <div className="flex flex-col gap-3 mt-[-10px] items-center justify-center">
-                <span className='flex flex-col  '> <input type="text" placeholder="Username" value={username} className="w-[250px] px-3 rounded-xl py-[7px] bg-[#34244c] border-[1px] text-gray-400" onChange={(e) => setusername(e.target.value)} />
-                {errors.username && <span className='text-red-600 ml-[15px] mt-[5px]'>{errors.username}</span>}
-              </span>
+
                <span className='flex flex-col'> <input type="Password" placeholder="Password" value={password} className="w-[250px] px-3 rounded-xl py-[7px] bg-[#34244c] border-[1px] text-gray-400" onChange={(e) => setpassword(e.target.value)} />
                 {errors.password && <span className='text-red-600 ml-[15px] mt-[5px]'>{errors.password}</span>}
               </span>
-                <span className='flex flex-col'> <input type="text" placeholder="ID NO" value={Idno} className="w-[250px] px-3 rounded-xl py-[7px] bg-[#34244c] border-[1px] text-gray-400" onChange={(e) => setIdno(e.target.value)} />
-                {errors.Idno && <span className='text-red-600 ml-[15px] mt-[5px]'>{errors.Idno}</span>}
+                 <span className='flex flex-col'> <input type="Password" placeholder="Confirm Password" value={Confirmpassword} className="w-[250px] px-3 rounded-xl py-[7px] bg-[#34244c] border-[1px] text-gray-400" onChange={(e) => setConfirmpassowrd(e.target.value)} />
+                {errors.Confirmpassword && <span className='text-red-600 ml-[15px] mt-[5px]'>{errors.Confirmpassword}</span>}
               </span>
               </div>
 
